9219e304d55f4cce3ad4b8a9c10b34353eb8012d
SudeepKarki <41881970+SudeepKarki@users.noreply.github.com>
2021-03-03 09:18:59 +0545

    Error Page
    Error Page

--- src/components/pages/page.js
+++ src/components/pages/page.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import ErrorPage from "../parts/errorPage/ErrorPage";
 import Footer from "../parts/footer/footer";
 import HeaderNav from "../parts/header/header";
 import Loader from "../parts/loader/Loader";
@@ -12,31 +13,39 @@ const Landing = React.lazy(() => import("./landing/landing"));
 const AboutUs = React.lazy(() => import("./aboutUs/aboutUs"));
 const Contact = React.lazy(() => import("./contact/contact"));
 const ProjectPage = React.lazy(() => import("./projects/projects"));
+// const ErrorPage = React.lazy(() => import("../parts/errorPage/ErrorPage"));
 
 function Pages() {
   return (
     <>
       <HeaderNav />
-      <Route path="/" exact>
-        <Suspense fallback={<Loader />}>
-          <Landing />
-        </Suspense>
-      </Route>
-      <Route path="/about-us">
-        <Suspense fallback={<Loader />}>
-          <AboutUs />
-        </Suspense>
-      </Route>
-      <Route path="/projects">
-        <Suspense fallback={<Loader />}>
-          <ProjectPage />
-        </Suspense>
-      </Route>
-      <Route path="/contact-us">
-        <Suspense fallback={<Loader />}>
-          <Contact />
-        </Suspense>
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <Suspense fallback={<Loader />}>
+            <Landing />
+          </Suspense>
+        </Route>
+        <Route path="/about-us">
+          <Suspense fallback={<Loader />}>
+            <AboutUs />
+          </Suspense>
+        </Route>
+        <Route path="/projects">
+          <Suspense fallback={<Loader />}>
+            <ProjectPage />
+          </Suspense>
+        </Route>
+        <Route path="/contact-us">
+          <Suspense fallback={<Loader />}>
+            <Contact />
+          </Suspense>
+        </Route>
+        <Route>
+          <Suspense fallback={<Loader />}>
+            <ErrorPage />
+          </Suspense>
+        </Route>
+      </Switch>
       {/* <Route path="/" exact component={Landing} /> */}
       {/* <Route path="/about-us" component={AboutUs} /> */}
       {/* <Route path="/projects" component={ProjectPage} /> */}
